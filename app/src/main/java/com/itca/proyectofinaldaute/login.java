d261e3e6dc325bb2d1dbdf45d6c875e612b4590a
NoelSorianoC <82602830+NoelSorianoC@users.noreply.github.com>
2021-11-09 09:56:03 -0600

    menu
    creacion de menu

--- app/src/main/java/com/itca/proyectofinaldaute/login.java
+++ app/src/main/java/com/itca/proyectofinaldaute/login.java
@@ -1,6 +1,34 @@
 package com.itca.proyectofinaldaute;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
 
-public class login extends Activity {
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class login extends AppCompatActivity {
+
+    private Button btniniciar;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        btniniciar=findViewById(R.id.btniniciar);
+
+        btniniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent home = new Intent(getApplicationContext(),ActivityMenu.class);
+                startActivity(home);
+            }
+        });
+    }
 }
---
+++ app/src/main/java/com/itca/proyectofinaldaute/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.itca.proyectofinaldaute.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Informacion de Usuario");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
