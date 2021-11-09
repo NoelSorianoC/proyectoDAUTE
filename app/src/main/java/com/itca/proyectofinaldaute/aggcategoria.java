56c2fd1db8d86337d25b410fa6588a43f8d22228
AntonioCampos19 <lpz.campos19@gmail.com>
2021-11-09 16:50:45 -0600

    Creacion de vistas categorias
    integracion de  vistas categorias

---
+++ app/src/main/java/com/itca/proyectofinaldaute/aggcategoria.java
@@ -0,0 +1,13 @@
+package com.itca.proyectofinaldaute;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class aggcategoria extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aggcategoria);
+    }
+}
