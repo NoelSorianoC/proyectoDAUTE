f1c357285765d43242b4d6e8af1ce36a1a2ce7c7
NoelSorianoC <82602830+NoelSorianoC@users.noreply.github.com>
2021-11-15 11:47:38 -0600

    registrar
    clases java

--- app/src/main/java/com/itca/proyectofinaldaute/registrar_us.java
+++ app/src/main/java/com/itca/proyectofinaldaute/registrar_us.java
@@ -3,12 +3,29 @@ package com.itca.proyectofinaldaute;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class registrar_us extends AppCompatActivity {
-
+private Button reg;
+    private EditText mail, contra;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_us);
+
+
+
+        reg = findViewById(R.id.reg);
+        mail = findViewById(R.id.mail);
+        contra = findViewById(R.id.contra);
+
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               // login(); nombre del metodo
+            }
+        });
     }
-}
+    }
