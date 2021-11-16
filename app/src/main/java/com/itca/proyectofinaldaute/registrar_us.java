ae2336f153165c34a5a3fd35b49dddf9a3e8ebb8
NoelSorianoC <82602830+NoelSorianoC@users.noreply.github.com>
2021-11-16 11:36:59 -0600

    registrar

--- app/src/main/java/com/itca/proyectofinaldaute/registrar_us.java
+++ app/src/main/java/com/itca/proyectofinaldaute/registrar_us.java
@@ -1,22 +1,34 @@
 package com.itca.proyectofinaldaute;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class registrar_us extends AppCompatActivity {
 private Button reg;
     private EditText mail, contra;
+
+    FirebaseAuth firebaseAuth;
+    FirebaseAuth.AuthStateListener mAuthListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_us);
 
 
-
+        firebaseAuth = FirebaseAuth.getInstance();
         reg = findViewById(R.id.reg);
         mail = findViewById(R.id.mail);
         contra = findViewById(R.id.contra);
@@ -24,8 +36,27 @@ private Button reg;
         reg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               // login(); nombre del metodo
+               // registro(); nombre del metodo
             }
         });
     }
+    public void registro() {
+        String email = mail.getText().toString().trim();
+        String password = contra.getText().toString();
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(registrar_us.this, "EL REGISTRO FUE CORRECTO", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(registrar_us.this, login.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    String e = task.getException().toString();
+                    Toast.makeText(registrar_us.this, e, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
     }
