daf4cbfcd40a85f301ec7ca4454dc94e4a55383b
AntonioCampos19 <83886129+AntonioCampos19@users.noreply.github.com>
2021-11-17 15:44:53 -0600

    clase java singleton

--- app/src/main/java/com/itca/proyectofinaldaute/aggcategoria.java
+++ app/src/main/java/com/itca/proyectofinaldaute/aggcategoria.java
@@ -1,18 +1,43 @@
 package com.itca.proyectofinaldaute;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class aggcategoria extends AppCompatActivity {
     private ImageView back;
+    private EditText id,nombre;
+    private Spinner estado;
+    private Button guardar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aggcategoria);
 
         back = findViewById(R.id.volver);
+        id = findViewById(R.id.cod_cat);
+        nombre = findViewById(R.id.nom_cat);
+        estado= findViewById(R.id.stado_c);
+        guardar = findViewById(R.id.guardarcat);
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -21,4 +46,54 @@ public class aggcategoria extends AppCompatActivity {
             }
         });
     }
+
+    private void guardarCategoria (final Context context, final int id_cat, final String name_cat, final int est_cat){
+        String url = "https://defunctive-loran.000webhostapp.com/guardarCategoria.php";
+
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>(){
+
+            @Override
+            public void onResponse(String response) {
+                JSONObject requestJSON;
+                try {
+                    requestJSON = new JSONObject(response.toString());
+                    String estado = requestJSON.getString("estado");
+                    String mensaje = requestJSON.getString("mensaje");
+
+                    if(estado.equals("1")){
+                        Toast.makeText(getApplicationContext(), mensaje, Toast.LENGTH_SHORT).show();
+                    }else if(estado.equals("2")){
+                        Toast.makeText(getApplicationContext(), mensaje, Toast.LENGTH_SHORT).show();
+                    }
+
+
+
+                }catch (JSONException e){
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "No se puedo guardar. \n" + "Intentelo más tarde.", Toast.LENGTH_SHORT).show();
+            }
+        }){
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String>  map = new HashMap<>();
+                map.put("Content-Type", "application/json; charset=utf-8");
+                map.put("Accept", "application/json");
+                map.put("id", String.valueOf(id_cat));
+                map.put("nombre", name_cat);
+                map.put("estado", String.valueOf(est_cat));
+                return map;
+
+            }
+
+        };
+
+        MySingleton.getInstance(context).addToRequestQueue(request);
+
+    }
+
 }
